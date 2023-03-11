2653c0f109009faa7592a963c8d51a8bbefab9a2
Hariharan Ananth <72250173+hariharan-tech@users.noreply.github.com>
2023-03-12 01:33:05 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-# count = 0
-path = "D:\SSN ECE\sem6\ml_lab\mini_project_ML"
-for root, dirs, files in os.walk(path):
-    for file in files:
-        if ".ttf" in file:
-        # if "reep" in file:
-            print(root,file)
-# print(count)
